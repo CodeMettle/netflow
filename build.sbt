e6f05de848994bb776fd49307f223bccf09fa2c0
Steven Scott <steven@codemettle.com>
2020-04-22 15:47:40 -0400

    release plugin didn't build cross versions
    it didn't try to pgp sign, either

--- build.sbt
+++ build.sbt
@@ -1,5 +1,3 @@
-import xerial.sbt.Sonatype.GitHubHosting
-
 lazy val `netflow-stream-lib` = project
   .settings(Settings.common)
   .settings(
@@ -62,15 +60,3 @@ lazy val `netflow-receiver` = project
 Global / onLoad += { (s: State) =>
   "project netflow-receiver" :: s
 }
-
-releaseCrossBuild in ThisBuild := true
-
-releasePublishArtifactsAction in ThisBuild := PgpKeys.publishSigned.value
-
-publishMavenStyle in ThisBuild := true
-
-licenses in ThisBuild := Seq("APL2" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt"))
-
-sonatypeProjectHosting in ThisBuild := Some(GitHubHosting("CodeMettle", "netflow", "steven@codemettle.com"))
-
-publishTo in ThisBuild := sonatypePublishTo.value
--- project/Settings.scala
+++ project/Settings.scala
@@ -1,5 +1,9 @@
+import com.jsuereth.sbtpgp.SbtPgp.autoImport.PgpKeys
 import sbt.Keys._
 import sbt._
+import sbtrelease.ReleasePlugin.autoImport.{releaseCrossBuild, releasePublishArtifactsAction}
+import xerial.sbt.Sonatype.GitHubHosting
+import xerial.sbt.Sonatype.autoImport.{sonatypeProjectHosting, sonatypePublishTo}
 
 object Settings {
   final val common = Seq[Setting[_]](
@@ -7,6 +11,12 @@ object Settings {
     crossScalaVersions := Versions.scala,
     scalaVersion := crossScalaVersions.value.head,
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-Xcheckinit", "-encoding", "utf8", "-feature"),
-    javacOptions ++= Seq("-Xlint:deprecation")
+    javacOptions ++= Seq("-Xlint:deprecation"),
+    releaseCrossBuild := true,
+    releasePublishArtifactsAction := PgpKeys.publishSigned.value,
+    publishMavenStyle := true,
+    licenses := Seq("APL2" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt")),
+    sonatypeProjectHosting := Some(GitHubHosting("CodeMettle", "netflow", "steven@codemettle.com")),
+    publishTo := sonatypePublishTo.value
   )
 }
